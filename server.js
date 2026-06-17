58af6ad3c2f87febc087fbf9fc14514e7b6f9683
manuliberalBB <manuel@bitabit.tech>
2026-06-17 11:13:11 -0300

    add client url cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
 }));
 app.use(express.json());
